cb4a8dfa18ec1e1850717ec3b1a856385ae48e02
taahahussainkhan <taaha128@gmail.com>
2023-03-29 23:52:55 +0500

    Virtual DOM

---
+++ jsx-syntax/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import home from './home';
+function App() {
+  return (//JSX syntax
+    <div>
+        <home />
+        <div>
+          Taaha
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ jsx-syntax/src/home.js
@@ -0,0 +1,13 @@
+const home = () => {//Components
+    return ( 
+        <div> 
+            Good Morning, Hope you are doing well.
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </div>
+        
+    );
+}
+export default home
