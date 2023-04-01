835c346fd2b1a6022c9d986cec17281c2ff63273
taahahussainkhan <taaha128@gmail.com>
2023-04-01 15:22:29 +0500

    finalf

---
+++ state_vs_props/src/Profile.js
@@ -0,0 +1,48 @@
+import React from "react";
+export default class Profile  extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            name:'',
+            email:''
+        }
+    }
+    updateState(){
+        this.setState({
+            name:'Taaha Hussain Khan',
+            email:'taaha128@gmail.com',
+            age:'21'
+        })
+    }
+    revertState(){
+        this.setState({
+            name:'',
+            email:'',
+            age:''
+        })
+    }
+    render() {
+        return (
+             <div>
+                <h1>---- S T A T E      P R A C T I C E ----</h1>
+                <h4>
+                    Name: {this.state.name}
+                </h4>
+                <h6>
+                    Email: {this.state.email}
+                </h6>
+                <h6>
+                    Age: {this.state.age}
+                </h6>
+                <button onClick={()=>{
+                    this.updateState()
+                }}>Update</button>
+
+                <button onClick={()=>{
+                    this.revertState()
+                }}>Revert</button>
+             </div>
+       
+        );
+    }
+}
